e2eb135589bd6cb347ff2c42c9ce320b05944b53
kevencript <kevencript@gmail.com>
2023-03-07 22:46:38 -0300

    Final Test: Expected change

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Example Go App"))
+		w.Write([]byte("<h1>Changing GO Server</h1>"))
 	}) 
 
 	http.ListenAndServe(":8080", nil)
